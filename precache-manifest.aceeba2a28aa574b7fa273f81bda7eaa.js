2171966a20c90811e0159324b15ad9ae6a70acd7
leticiamendonca <leticia.mendonca68@gmail.com>
2020-01-04 15:53:32 +0530

    Updates

---
+++ precache-manifest.aceeba2a28aa574b7fa273f81bda7eaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fea2656d2f9e0bdacda4ebe3bf6fed",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fcb3443d814ed319b735",
+    "url": "/robofriends/static/css/main.37b447b2.chunk.css"
+  },
+  {
+    "revision": "5f5c93159adf24522178",
+    "url": "/robofriends/static/js/2.8d7c7b1f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.8d7c7b1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fcb3443d814ed319b735",
+    "url": "/robofriends/static/js/main.daa71eea.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
